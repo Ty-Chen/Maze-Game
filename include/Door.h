099bec95536d008836c129c547c96b6829c47677
Ty-Chen <369494003@qq.com>
2021-07-18 13:02:29 +0800

    Add door and wall

---
+++ include/Door.h
@@ -0,0 +1,13 @@
+﻿#pragma once
+#include "Room.h"
+class Door : public MapSite
+{
+public:
+	Door(Room* pOneSide, Room* pOtherSide);
+	virtual void Enter();
+	Room* GetOtherSide(Room* pNowRoom);
+
+private:
+	Room* m_Room[2];
+	bool  m_bOpen;
+};
---
+++ include/Wall.h
@@ -0,0 +1,11 @@
+﻿#pragma once
+#include "MapSite.h"
+
+class Wall
+{
+public:
+	Wall();
+	~Wall();
+
+	virtual void Enter();
+};
